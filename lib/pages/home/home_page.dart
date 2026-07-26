0eb4308a941b66aa8713ef453225d52c2e9e28d2
RamiroAlves <ramiro.junior25@gmail.com>
2026-07-26 11:37:15 -0300

    feat: create features section structure

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_landing/pages/home/sections/features/features_setion.dart';
 import 'package:flutter_landing/pages/home/sections/hero_section.dart';
 import 'package:flutter_landing/pages/home/sections/navbar_section.dart';
 
@@ -13,6 +14,7 @@ class HomePage extends StatelessWidget {
           children: [
             NavbarSection(),
             HeroSection(),
+            FuturesSection(),
           ],
         ),
       )
---
+++ lib/pages/home/sections/features/features_setion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FuturesSection extends StatelessWidget {
+  const FuturesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
